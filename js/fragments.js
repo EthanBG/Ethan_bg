0071b5fdee76bb10a9455bcf9c30a729daff2224
Ethan <ebeauvaisguibert@gmail.com>
2019-06-14 20:06:05 +0200

    HEADER

--- js/fragments.js
+++
@@ -1,6 +0,0 @@
-function inject(name){
-  var link = document.querySelector('link[rel="import"]');
-  var content = link.import;
-  var element = content.querySelector('#'+name);
-  document.body.appendChild(element.cloneNode(true));
-}
